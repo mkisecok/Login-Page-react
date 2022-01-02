2e31b5a853470b22737f57bef68f48c755e3507c
Mkisecok <mahmutkisecok@gmail.com>
2022-01-02 21:29:15 +0100

    Done login component design

--- login/src/components/Login.js
+++ login/src/components/Login.js
@@ -1,10 +1,11 @@
-import { Avatar, Button, Checkbox, FormControlLabel, Grid, Paper, TextField } from '@mui/material'
+import { Avatar, Button, Checkbox, FormControlLabel, Grid, Link, Paper, TextField, Typography } from '@mui/material'
 import React from 'react'
 import LockOutlinedIcon from '@mui/icons-material/LockOutlined';
 
 function Login() {
     const paperStyle={padding:20, height:'70vh', width:300, margin:'auto'};
-    const avatarStyle={backgroundColor:'#1bbd7d'}
+    const avatarStyle={backgroundColor:'#1bbd7d'};
+    const btnStyle={margin:'8px 0'}
     return (
         <div>
             <Grid>
@@ -12,12 +13,17 @@ function Login() {
                     <Grid align='center'>  
                         <Avatar style={avatarStyle}><LockOutlinedIcon/></Avatar>
                         <h2>Sign In</h2>  
-                     </Grid>
-                     <TextField id="standard-basic" label="Username" placeholder='Enter Username' variant="standard" fullWidth  required/>
-                     <TextField id="standard-basic" label="Password" placeholder='Enter Password' type='password' variant="standard" fullWidth  required/>
-                     <FormControlLabel control={<Checkbox name='checkedB' color='primary' align=''/>} label="Remember me" />
-                     <Button type='submit' color='primary' variant="contained" fullWidth>Sign in</Button>
-
+                    </Grid>
+                    <TextField id="standard-basic" label="Username" placeholder='Enter Username' variant="standard" fullWidth  required/>
+                    <TextField id="standard-basic" label="Password" placeholder='Enter Password' type='password' variant="standard" fullWidth  required/>
+                    <FormControlLabel control={<Checkbox name='checkedB' color='primary' align=''/>} label="Remember me" />
+                    <Button type='submit' color='primary' variant="contained" style={btnStyle} fullWidth>Sign in</Button>
+                    <Typography>
+                         <Link href="#" >Forget Password?</Link>
+                    </Typography>
+                    <Typography>Do you have an account?
+                         <Link href="#" >Sign Up</Link>
+                    </Typography>
 
                 </Paper>
             </Grid>
