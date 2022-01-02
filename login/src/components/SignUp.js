7874ba746f3d6674f0ac687c9c17fab0510bf552
Mkisecok <mahmutkisecok@gmail.com>
2022-01-02 22:26:12 +0100

    Done signUp component

--- login/src/components/SignUp.js
+++ login/src/components/SignUp.js
@@ -1,6 +1,11 @@
 import React from 'react'
-import {Grid,Paper,Avatar,Typography,TextField,Button} from '@mui/material'
+import {Grid,Paper,Avatar,Typography,TextField,Button,Checkbox} from '@mui/material'
 import AddCircleOutlineOutlinedIcon from '@mui/icons-material/AddCircleOutlineOutlined';
+import Radio from '@mui/material/Radio';
+import RadioGroup from '@mui/material/RadioGroup';
+import FormControlLabel from '@mui/material/FormControlLabel';
+import FormControl from '@mui/material/FormControl';
+import FormLabel from '@mui/material/FormLabel';
 import '../App.css'
 
 function SignUp() {
@@ -20,8 +25,22 @@ function SignUp() {
                  <TextField label='Name' variant="standard" placeholder='Enter your name' fullWidth  required/>
                  <TextField label='Email' variant="standard" placeholder='Enter your Email'fullWidth  required/>
                  <TextField label='Phone Number' variant="standard" placeholder='Enter your Phone' fullWidth  />
+                 <FormControl component="fieldset" style={{marginTop:5}}>
+                <FormLabel component="legend">Gender</FormLabel>
+                <RadioGroup
+                    aria-label="gender"
+                    defaultValue="female"
+                    name="radio-buttons-group"
+                    style={{display:'initial'}}
+                >
+                    <FormControlLabel value="female" control={<Radio color="success"/>} label="Female"  />
+                    <FormControlLabel value="male" control={<Radio color="success"/>} label="Male" />
+                    <FormControlLabel value="other" control={<Radio color="success"/>} label="Other" />
+                </RadioGroup>
+                </FormControl>
                  <TextField label='Password' variant="standard" placeholder='Enter your Password'type='password' fullWidth  required/>
                  <TextField label='Confirm Password' type='password' placeholder='Confirm your Password'variant="standard" fullWidth  required/>
+                 <FormControlLabel control={<Checkbox />} label="I accept the terms and conditions" />
                  <Button type='submit' color='primary' variant='contained'>Sign Up</Button>
              </form>
              
