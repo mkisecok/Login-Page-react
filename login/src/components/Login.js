5074997eace33a693a82dd87c0e50aa0a001cb24
Mkisecok <mahmutkisecok@gmail.com>
2022-01-02 20:56:33 +0100

    feat:Change the style of avatar component

--- login/src/components/Login.js
+++ login/src/components/Login.js
@@ -1,14 +1,19 @@
-import { Grid, Paper } from '@mui/material'
+import { Avatar, Grid, Paper } from '@mui/material'
 import React from 'react'
-
+import LockOutlinedIcon from '@mui/icons-material/LockOutlined';
 
 function Login() {
-    const paperStyle={padding:20, height:'70vh', width:300, margin:'auto'}
+    const paperStyle={padding:20, height:'70vh', width:300, margin:'auto'};
+    const avatarStyle={backgroundColor:'#1bbd7d'}
     return (
         <div>
             <Grid>
                 <Paper  elevation={10} style={paperStyle}>
-                Sign In
+                    <Grid align='center'>  
+                        <Avatar style={avatarStyle}><LockOutlinedIcon/></Avatar>
+                        <h2>Sign In</h2>  
+                     </Grid>
+                  
                 </Paper>
             </Grid>
         </div>
