2399a51f7939dfd6e1193eaf69833bf75a744d46
Mkisecok <mahmutkisecok@gmail.com>
2022-01-03 09:12:12 +0100

    Add Yup package validation to SignUp

--- login/src/components/SignUp.js
+++ login/src/components/SignUp.js
@@ -6,7 +6,10 @@ import RadioGroup from '@mui/material/RadioGroup';
 import FormControlLabel from '@mui/material/FormControlLabel';
 import FormControl from '@mui/material/FormControl';
 import FormLabel from '@mui/material/FormLabel';
-import {Formik,Field,Form} from 'formik'
+import {Formik,Field,Form, ErrorMessage} from 'formik'
+import { FormHelperText } from '@mui/material';
+import * as Yup from 'yup'
+
 
 function SignUp() {
     const paperStyle={padding:20, height:'70vh', width:300, margin:'auto'};
@@ -20,8 +23,22 @@ function SignUp() {
         confirmPassword:'',
         termsAndConditions:false,
     }
+    const validationSchema=Yup.object().shape({
+        name:Yup.string().min(3, 'Your name is too short').required(),
+        email:Yup.string().email('Enter valid email').required(),
+        phoneNumber:Yup.number().typeError('Enter valid Phone Number'),
+        gender:Yup.string().oneOf(['male','female','other']).required(),
+        password:Yup.string().min(8,'Password must min 8 character').required(),
+        confirmPassword:Yup.string().oneOf([Yup.ref('password')], 'Thats not same with your password').required(),
+        termsAndConditions:Yup.string().oneOf(['true'], 'Accept terms and conditions').required()
+    })
     const onSubmit=(values,props)=>{
         console.log(values);
+        console.log(props);
+        setTimeout(()=>{
+            props.resetForm()
+            props.setSubmitting(false)
+        },2000)
     }
     return (
      <Grid>
@@ -33,30 +50,33 @@ function SignUp() {
                  <h2 style={{margin:0}}> Sign Up</h2>
                  <Typography variant='caption'>Please fill this form to create an account</Typography>
              </Grid>
-            <Formik initialValues={initialValues} onSubmit={onSubmit}>
+            <Formik initialValues={initialValues} validationSchema={validationSchema} onSubmit={onSubmit}>
                 {(props)=>(
                 <Form>
  
-                 <Field as={TextField} label='Name' name='name' variant="standard" placeholder='Enter your name' fullWidth  required/>
-                 <Field as={TextField} label='Email' name='email' variant="standard" placeholder='Enter your Email'fullWidth  required/>
-                 <TextField label='Phone Number' name='phoneNumber' variant="standard" placeholder='Enter your Phone' fullWidth  />
+                 <Field as={TextField} label='Name' name='name' variant="standard" placeholder='Enter your name' fullWidth  required helperText={<ErrorMessage  name='name'/>}/>
+                 <Field as={TextField} label='Email' name='email' variant="standard" placeholder='Enter your Email'fullWidth  required helperText={<ErrorMessage  name='email'/>}/>
+                 <Field as={TextField} label='Phone Number' name='phoneNumber' variant="standard" placeholder='Enter your Phone' fullWidth helperText={<ErrorMessage  name='phoneNumber'/>} />
                  <FormControl component="fieldset" style={{marginTop:5}}>
                 <FormLabel component="legend">Gender</FormLabel>
                 <Field as={RadioGroup}
                     aria-label="gender"
-                    defaultValue="female"
-                    name="radio-buttons-group"
+                   
+                    name="gender"
                     style={{display:'initial'}}
+                   
                 >
                     <FormControlLabel value="female" control={<Radio color="success"/>} label="Female"  />
                     <FormControlLabel value="male" control={<Radio color="success"/>} label="Male" />
                     <FormControlLabel value="other" control={<Radio color="success"/>} label="Other" />
                 </Field>
                 </FormControl>
-                 <Field as={TextField} label='Password' name='password' variant="standard" placeholder='Enter your Password'type='password' fullWidth  required/>
-                 <Field as={TextField} label='Confirm Password' name='confirmPassword' type='password' placeholder='Confirm your Password'variant="standard" fullWidth  required/>
-                 <FormControlLabel control={<Field as={Checkbox}  name='termsAndConditions'/>} label="I accept the terms and conditions" />
-                 <Button type='submit' color='primary' variant='contained'>Sign Up</Button>
+                <FormHelperText> <ErrorMessage  name="gender"/></FormHelperText>
+                 <Field as={TextField} label='Password' name='password' variant="standard" placeholder='Enter your Password'type='password' fullWidth  required  helperText={<ErrorMessage  name='password'/>}/>
+                 <Field as={TextField} label='Confirm Password' name='confirmPassword' type='password' placeholder='Confirm your Password'variant="standard" fullWidth  required  helperText={<ErrorMessage  name='confirmPassword'/>}/>
+                 <FormControlLabel control={<Field as={Checkbox}  name='termsAndConditions' />} label="I accept the terms and conditions" />
+                 <FormHelperText> <ErrorMessage  name='termsAndConditions'/></FormHelperText>
+                 <Button type='submit' color='primary' disabled={props.isSubmitting} variant='contained'>{props.isSubmitting?'Loading':'Sign Up'}</Button>
              
 
                     </Form>
